bb137e22ef12b9f15264678fd911c99e9cf42918
Vivek Srivastava <155307110+viveksrivastava111@users.noreply.github.com>
2026-07-03 11:34:12 +0530

    Delete MobileApp.java

--- MobileApp.java
+++
@@ -1,6 +0,0 @@
-public class MobileApp implements Observer {
-
-    public void update(String stockName, double price) {
-        System.out.println("Mobile App: " + stockName + " price changed to " + price);
-    }
-}
