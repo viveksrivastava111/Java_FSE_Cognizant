80abac53dd20acc38c727e67a834fc177d05f2b2
Vivek Srivastava <155307110+viveksrivastava111@users.noreply.github.com>
2026-07-03 11:35:55 +0530

    Delete WebApp.java

--- WebApp.java
+++
@@ -1,6 +0,0 @@
-public class WebApp implements Observer {
-
-    public void update(String stockName, double price) {
-        System.out.println("Web App: " + stockName + " price changed to " + price);
-    }
-}
