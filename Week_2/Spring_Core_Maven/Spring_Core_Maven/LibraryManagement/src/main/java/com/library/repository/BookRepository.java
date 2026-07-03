b3f0a97b87f095188f8cbb37ca5cb32083074090
Vivek Srivastava <155307110+viveksrivastava111@users.noreply.github.com>
2026-07-03 11:52:28 +0530

    Add files via upload

---
+++ Week_2/Spring_Core_Maven/Spring_Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,19 @@
+// package com.library.repository;
+
+// public class BookRepository {
+
+//     public void displayBooks() {
+//         System.out.println("Books are fetched from BookRepository");
+//     }
+// }
+
+
+
+package com.library.repository;
+
+public class BookRepository {
+
+    public void getBooks() {
+        System.out.println("Book data is coming from BookRepository");
+    }
+}
---
+++ Week_2/Spring_Core_Maven/Spring_Core_Maven/LibraryManagement_Exercise_5/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void getBooks() {
+        System.out.println("Books are fetched from BookRepository");
+    }
+}
