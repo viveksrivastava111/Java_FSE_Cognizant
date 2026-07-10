870f52ed72cb360af5b53771564aa2838db6b0f8
Vivek Srivastava <155307110+viveksrivastava111@users.noreply.github.com>
2026-07-10 23:44:00 +0530

    Add files via upload

---
+++ Week_3/Create a Spring Web Project using Maven/spring-learn/src/test/java/com/cognizant/springlearn/SpringLearnApplicationTests.java
@@ -0,0 +1,21 @@
+package com.cognizant.springlearn;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+        // Checks whether the Spring context loads successfully
+
+    }
+
+
+}
---
+++ Week_3/Create a Spring Web Project using Maven/spring-learn/target/classes/application.properties
@@ -0,0 +1,6 @@
+# Application Name
+spring.application.name=spring-learn
+
+
+# Server Port
+server.port=8080
